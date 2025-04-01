3daf9141a2c444cb1177aa925c8e0f02616af05d
Marianne Oliveira <mariannerodrigues0806@gmail.com>
2025-03-31 22:19:01 -0300

    Iniciando a tela de entrar

---
+++ malams/resources/views/entrar.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Salão de Beleza</title>
+    <link rel="stylesheet" href="/css/entrar.css">
+</head>
+<body>
+
+    <header>
+        <div class="logo">LOGO</div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+        <div class="social-icons">
+            <a href="#"><img src="facebook-icon.png" alt="Facebook"></a>
+            <a href="#"><img src="instagram-icon.png" alt="Instagram"></a>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="form-container">
+                <h4>Entre</h4>
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Senha">
+                <button>Acessar</button>
+            </div>
+            <div class="image-container">
+                <img src="/img/entrar.jpg" alt="Mulher sorrindo">
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <p>Faça parte da nossa família</p>
+        <div class="footer-links">
+            <div class="link-item">
+                <a href="#">Nossos Profissionais</a>
+                <img src="profissionais.jpg" alt="Profissionais">
+            </div>
+            <div class="link-item">
+                <a href="#">Localização</a>
+                <img src="localizacao.jpg" alt="Localização">
+            </div>
+            <div class="link-item">
+                <a href="#">Avaliações</a>
+                <img src="avaliacoes.jpg" alt="Avaliações">
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
