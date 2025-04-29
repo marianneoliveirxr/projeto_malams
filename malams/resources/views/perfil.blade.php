6c3fb08aefdf2fc5721563bf2c18f18bfd1a675a
luigicazzeri <luigi.cazzeri0310@gmail.com>
2025-04-29 20:41:12 -0300

    Tela de Perfil

---
+++ malams/resources/views/perfil.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de perfil</title>
+    <link rel="stylesheet" href="/css/perfil.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Lora:wght@400&display=swap" rel="stylesheet">
+    
+</head>
+
+<body>
+    <header>
+        <img class="logo" src="/img/malamslogo.png" alt="logo">
+        <nav>
+            <ul>
+                <li><a class="home" href="#">Home</a></li>
+                <li><a class="servicos" href="#">Serviços</a></li>
+            </ul>
+        </nav>
+        <div class="social-icons">
+            <img src=/img/facebook.png alt="Facebook">
+            <img src=/img/instagram.png alt="Instagram">
+        </div>
+    </header>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
