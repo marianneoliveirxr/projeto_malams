107bff5fc9adf39e423db849a216c8df51c506f0
luigicazzeri <luigi.cazzeri0310@gmail.com>
2025-05-12 22:22:27 -0300

    Arrumando back

--- malams/app/Http/Controllers/PerfilController.php
+++ malams/app/Http/Controllers/PerfilController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class PerfilController extends Controller
 {
-    //
+    public function index()
+    {
+        $perfil = Perfil::all();
+                    //nome da view       //nome do atributo $cliente sem $
+       return view('perfil',compact('perfil'));
+    }
+
+    public function store(Request $request)
+    {
+        $perfil = new Perfil();
+            $perfil ->  nome = $request -> nomeCliente;
+            $perfil ->  cpf = $request -> cpfCliente;
+            $perfil ->  data = $request -> dataNascimento;
+            $perfil ->  celular = $request -> celularCliente;
+            $perfil ->  email = $request -> emailCliente;
+
+            $perfil -> save ();
+
+            return redirect ('/perfil');
+    }
 }
--- malams/app/Models/Perfil.php
+++ malams/app/Models/Perfil.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Perfil extends Model
+class Cliente extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'clientes'; // Nome da tabela
+
+    public $fillable = ['id', 'nomeCliente', 'cpfCliente', 'dataNascimento', 'celularCliente', 'emailCliente'];
+
+    public $timestamps = false;
 }
